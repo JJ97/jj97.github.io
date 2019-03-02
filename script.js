8d375b57b2bc999de7662a31f8102b1f840a3ca0
JJ97 <johnmjennings97@gmail.com>
2019-03-02 20:36:26 +0000

    js edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function main(){
-  let audio = document.getElementById("tunez");
-  audio.play();
-}
-window.onload = main;
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === "complete") {
+    document.getElementById("tunez").play();
+  }
+});
